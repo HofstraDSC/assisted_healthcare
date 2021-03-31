b7613dbbb709f3a27095eb36186b522528bf925d
AlvinKuruvilla <46870885+AlvinKuruvilla@users.noreply.github.com>
2021-03-31 18:18:09 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:assisted_healthcare/Pages/wrapper.dart';
+import 'package:assisted_healthcare/models/user.dart';
+import 'package:assisted_healthcare/services/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +11,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Wrapper());
+    return FutureBuilder(
+        // Initialize Firebase in Flutter
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          return StreamProvider<TheUser>.value(
+              // access data from TheUser
+              value: AuthService().user,
+              initialData: null,
+              child: MaterialApp(home: Wrapper()) // Show app
+              );
+        });
   }
 }
