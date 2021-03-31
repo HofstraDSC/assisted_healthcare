f33120af968332fab6647124eedf877befe4facc
AlvinKuruvilla <46870885+AlvinKuruvilla@users.noreply.github.com>
2021-03-31 18:27:21 -0400

    Add files via upload

---
+++ lib/user.dart
@@ -0,0 +1,14 @@
+class TheUser {
+  final String uid;
+// Controls what properties user has
+  TheUser({this.uid});
+}
+
+class UserData {
+  final String uid;
+  final String firstName;
+  final String lastName;
+  final String insurance;
+
+  UserData({this.uid, this.firstName, this.lastName, this.insurance});
+}
